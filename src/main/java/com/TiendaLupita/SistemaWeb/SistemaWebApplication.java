812574145fc1fc71ff3a3f1c2eaaca40cc2e7872
Luis <ldpumacondor@gmail.com>
2023-10-02 22:05:21 -0500

    configs

--- src/main/java/com/TiendaLupita/SistemaWeb/SistemaWebApplication.java
+++ src/main/java/com/TiendaLupita/SistemaWeb/SistemaWebApplication.java
@@ -9,6 +9,7 @@ public class SistemaWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaWebApplication.class, args);
 		System.out.println("hola mundo");
+		
 	}
 
 }
